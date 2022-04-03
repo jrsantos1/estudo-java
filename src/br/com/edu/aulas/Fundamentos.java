5db4bfd3326e467bd20220da979aa9ee09443ea8
Jhonatan Santos <48073565+jrsantos1@users.noreply.github.com>
2022-04-03 18:59:15 -0300

    adicionado arquivo fundamentos do java

---
+++ src/br/com/edu/aulas/Fundamentos.java
@@ -0,0 +1,21 @@
+package br.com.edu.aulas;
+
+public class Fundamentos {
+    public static void main(String[] args) { // metodo que executa o programa
+
+        // comentário
+
+        // como declarar constante
+        final double PI = 3.14;
+
+        // inferencia de tipo - não selecionar o tipo em sua inicialização
+
+        var idade = 13; // int
+        var saldoEmConta = 500.0; // Real
+        var nome = ""; // String
+        var EMaiorDeDezoito = true; // boolean
+
+        // wappers - são os objetos em java para cada tipo primitivo
+
+    }
+}
