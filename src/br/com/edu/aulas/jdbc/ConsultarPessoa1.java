5c4ac50734e54dfc345c28dbb7bb27fc56227174
Jhonatan Santos <48073565+jrsantos1@users.noreply.github.com>
2022-04-05 00:31:01 -0300

    adicionando exercicios de persistencia

---
+++ src/br/com/edu/aulas/jdbc/ConsultarPessoa1.java
@@ -0,0 +1,34 @@
+package br.com.edu.aulas.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultarPessoa1 {
+    public static void main(String[] args) throws SQLException {
+        Connection conexao = DBFactory.getConexao();
+
+        String comendoSQL = "SELECT * FROM pessoas";
+
+        Statement stmt = conexao.createStatement();
+        ResultSet resultado = stmt.executeQuery(comendoSQL);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        while(resultado.next()){
+            pessoas.add(new Pessoa(resultado.getInt("codigo"), resultado.getString("nome")));
+        }
+
+        for (Pessoa p: pessoas
+             ) {
+            System.out.println(p.getCodigo() + " " + p.getNome());
+        }
+
+        stmt.close();
+        conexao.close();
+
+    }
+}
---
+++ src/br/com/edu/aulas/jdbc/NovaPessoa.java
@@ -0,0 +1,28 @@
+package br.com.edu.aulas.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class NovaPessoa {
+    public static void main(String[] args) throws SQLException {
+
+        Connection conexao = DBFactory.getConexao();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite seu nome");
+        String nome = sc.next();
+
+        String comendoSQL = "INSERT INTO pessoas (nome) values (?)";
+
+        PreparedStatement stmt = conexao.prepareStatement(comendoSQL);
+        stmt.setString(1, nome);
+        stmt.execute();
+
+        System.out.println("usuario criado com sucesso");
+
+        conexao.close();
+
+    }
+}
