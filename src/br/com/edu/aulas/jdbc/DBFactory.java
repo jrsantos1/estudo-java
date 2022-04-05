097fee59bd47c6e6c66545dba30e41a5fc8e608b
Jhonatan Santos <48073565+jrsantos1@users.noreply.github.com>
2022-04-04 23:31:26 -0300

    iniciando estudos com banco de dados

---
+++ src/br/com/edu/aulas/jdbc/DBFactory.java
@@ -0,0 +1,20 @@
+package br.com.edu.aulas.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBFactory {
+
+    public static Connection getConexao() {
+        try {
+            String stringDeConexao = "jdbc:mysql://localhost:3306/curso_java?verifyServerCertificate=false&useSSL=true";
+            String user = "root";
+            String password = "root123";
+            return DriverManager.getConnection(stringDeConexao, user, password);
+        }catch (SQLException e){
+            //e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
