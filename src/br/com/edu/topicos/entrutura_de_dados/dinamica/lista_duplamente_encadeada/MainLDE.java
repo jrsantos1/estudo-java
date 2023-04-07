867d3af7580095b99a7b9e2d9f1d765653943fd8
jrsantos1 <jhonatanjonh@outlook.com>
2023-04-07 18:44:58 -0300

    add exercicios lista duplamente encadeada

---
+++ src/br/com/edu/topicos/entrutura_de_dados/dinamica/lista_duplamente_encadeada/MainLDE.java
@@ -0,0 +1,7 @@
+package br.com.edu.topicos.entrutura_de_dados.dinamica.lista_duplamente_encadeada;
+
+public class MainLDE {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
